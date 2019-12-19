d0bf2c7ea09ce6fccf3e560615feadcbdc5297f5
Harvey Jay Sison <38162086+Hjkun77@users.noreply.github.com>
2019-12-20 01:25:24 +0800

    Cobbie (#1)
    * init commit branch cobbie - skeleton divs and css and display function
    
    * mobile css, article 1 content
    
    * article 2,3
    
    * comments for added code and removed logs
    
    * add photos and text overlay
    
    * added exit button, fix some spacing, tiempos text font as link tag
    
    * media query for desktop

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -2,67 +2,78 @@ const overlay = document.getElementById("overlay");
 const home0 = document.getElementById("home0");
 const home1 = document.getElementById("home1");
 
+const article = document.getElementById("article");
+
 let count = 0;
 let overlayStatus = true;
 
 const onScrollDown = () => {
-    overlay.className = "animated fadeOut";
-    count = 5;
-    overlayStatus = false;
+  overlay.className = "animated fadeOut";
+  count = 5;
+  overlayStatus = false;
 };
 
-window.addEventListener('wheel', e => {
-    let delta = e.deltaY; // just to know if it is scroll wheel up or down
+window.addEventListener("wheel", e => {
+  let delta = e.deltaY; // just to know if it is scroll wheel up or down
 
-    if (delta < 0) {
-        count -= 1;
+  if (delta < 0) {
+    count -= 1;
+  } else if (delta > 0) {
+    // scroll down
+    count += 1;
+    overlay.className = "animated fadeOut";
+    overlayStatus = false;
+  }
 
-    } else if (delta > 0) {
-        // scroll down
-        count += 1;
-        overlay.className = "animated fadeOut";
-        overlayStatus = false;
-    }
-
-    if (count <= 0 && overlayStatus === false) {
-        overlay.className = "animated fadeInDown";
-        overlayStatus = true;
-    } else if (count >= 75) {
-        home1.style.display = "none";
-    } else if (count <= 25) {
-        home0.style.display = "block";
-    } else if (count >= 25) {
-        home0.style.display = "none";
-        home1.style.display = "block";
-    }
+  if (count <= 0 && overlayStatus === false) {
+    overlay.className = "animated fadeInDown";
+    overlayStatus = true;
+  } else if (count >= 75) {
+    home1.style.display = "none";
+  } else if (count <= 25) {
+    home0.style.display = "block";
+  } else if (count >= 25) {
+    home0.style.display = "none";
+    home1.style.display = "block";
+  }
 });
 
+(function() {
+  document.onmousemove = handleMouseMove;
+  function handleMouseMove(event) {
+    var eventDoc, doc, body;
 
-(function () {
-    document.onmousemove = handleMouseMove;
-    function handleMouseMove(event) {
-        var eventDoc, doc, body;
+    event = event || window.event; // IE-ism
+    const x = event.pageX;
+    const y = event.pageY;
+    // If pageX/Y aren't available and clientX/Y are,
+    // calculate pageX/Y - logic taken from jQuery.
+    // (This is to support old IE)
+    if (x == null && event.clientX != null) {
+      eventDoc = (event.target && event.target.ownerDocument) || document;
+      doc = eventDoc.documentElement;
+      body = eventDoc.body;
 
-        event = event || window.event; // IE-ism
-        const x = event.pageX;
-        const y = event.pageY;
-        // If pageX/Y aren't available and clientX/Y are,
-        // calculate pageX/Y - logic taken from jQuery.
-        // (This is to support old IE)
-        if (x == null && event.clientX != null) {
-            eventDoc = (event.target && event.target.ownerDocument) || document;
-            doc = eventDoc.documentElement;
-            body = eventDoc.body;
-
-            x = event.clientX +
-                (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-                (doc && doc.clientLeft || body && body.clientLeft || 0);
-            y = event.clientY +
-                (doc && doc.scrollTop || body && body.scrollTop || 0) -
-                (doc && doc.clientTop || body && body.clientTop || 0);
-        }
-        // Use event.pageX / event.pageY here
-        console.log(x, y);
-
+      x =
+        event.clientX +
+        ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
+        ((doc && doc.clientLeft) || (body && body.clientLeft) || 0);
+      y =
+        event.clientY +
+        ((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
+        ((doc && doc.clientTop) || (body && body.clientTop) || 0);
     }
+    // Use event.pageX / event.pageY here
+    console.log(x, y);
+  }
 })();
+
+// function for showing article
+const showArticle = articleNum => {
+  const article = document.getElementById(`article-${articleNum}`);
+  article.style.display = "block";
+};
+
+// show article on load for dev purposes
+document.onload = onScrollDown();
+document.onload = showArticle("1");
